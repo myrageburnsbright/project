4ca8991619e9c4480a0deceaa77960ff98d23745
LaInvasora <myrageburnsbright@gmail.com>
2025-03-29 01:00:18 -0400

    After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from goods.models import Categories, Product
+
+#admin.site.register(Categories)
+
+
+#admin.site.register(Product)
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,36 @@
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True,null=True)
+    price = models.DecimalField(default=0.00,max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00,max_digits=4, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} - Col {self.quantity}'
+    
