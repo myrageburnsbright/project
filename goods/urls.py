df2fde2b600be9e4cfa11321a84700e84e1a6e12
LaInvasora <myrageburnsbright@gmail.com>
2025-03-31 20:56:58 -0400

    Simple Search created

--- goods/urls.py
+++ goods/urls.py
@@ -6,8 +6,8 @@ from goods import views
 app_name ='goods'
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('product/<slug:product_slug>', views.product, name='product'),
-    
     path('<slug:category_slug>', views.catalog, name='index'),
     
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_list_or_404
 from goods.models import Product, Categories
 from django.core.paginator import Paginator
+from .utils import q_search
+from goods.utils import q_search
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug = None) :
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Product.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
-        goods = get_list_or_404(Product.objects.all().filter(category__slug=category_slug))
+        goods = Product.objects.all().filter(category__slug=category_slug)
+    
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+    goods = get_list_or_404(goods)
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
 
